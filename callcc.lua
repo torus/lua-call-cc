eae356ea7b9de7c8bb57b7c2f67ed00392c11621
torus <toru-github@torus.jp>
2009-05-08 14:11:28 -0700

    fix typo

--- callcc.lua
+++ callcc.lua
@@ -1,6 +1,6 @@
 -- Lua call/cc sample
 
--- This script tested on Lua 5.1.4 with coroutine.clone path
+-- This script tested on Lua 5.1.4 with coroutine.clone patch
 -- http://lua-users.org/lists/lua-l/2006-01/msg00652.html
 
 local pc
