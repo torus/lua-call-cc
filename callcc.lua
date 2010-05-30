804f2b5c559b58999c297ef2b7806ddd8dabe5e6
Toru Hisai <toru-github@torus.jp>
2010-05-29 17:50:19 -0700

    added license.

--- callcc.lua
+++ callcc.lua
@@ -2,6 +2,7 @@
 
 -- This script tested on Lua 5.1.4 with coroutine.clone patch
 -- http://lua-users.org/lists/lua-l/2006-01/msg00652.html
+-- License: MIT (same as Lua http://www.lua.org/license.html )
 
 local pc
 local continuation
